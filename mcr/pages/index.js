c4c13a594cda073c69c447e8712975328c8fbae9
AJ Raymond <antraymo@iu.edu>
2020-07-31 22:20:05 -0500

    recipe page

--- mcr/pages/index.js
+++ mcr/pages/index.js
@@ -2,6 +2,51 @@ import React from 'react';
 import Head from 'next/head';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import ModalHeader from 'react-bootstrap/ModalHeader';
+import ModalTitle from 'react-bootstrap/ModalTitle';
+import ModalBody from 'react-bootstrap/ModalBody';
+import ReactPlayer from 'react-player';
+
+
+const img ={
+  backgroundImage: 'url("./garage.jpg"), linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5))',
+  height: '90vh',
+  backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
+  position: 'relative',
+  marginBottom: '25px',
+}
+
+const text ={
+    textAlign: 'center',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    color: 'white',
+    width: '100%'
+}
+
+const recipe ={
+  marginBottom: '25px',
+}
+
+const button ={
+  background: '#000',
+  borderRadius: '10px',
+  border: 'none',
+  color: 'var(--white)',
+  fontWeight: 'bold',
+  width: '40vw',
+  padding: 15,
+  margin: 10,
+  textAlign: 'center',
+}
 
 export default class Home extends React.Component {
   constructor(props) {
@@ -15,7 +60,49 @@ export default class Home extends React.Component {
         <title>Man Cave Recipes</title>
       </Head>
       <Header />
-      <h1>Jim's Garage Sweet Garage</h1>
+      <div style={img}>
+        <div style={text}>
+          <h1>JIM'S GARAGE SWEET GARAGE</h1>
+          <p>Insert Catchy Tagline</p>
+        </div>
+      </div>
+
+      <div>
+        <h1>Latest Recipe</h1>
+
+        <CardGroup style={recipe}>
+        <Card>
+          <ReactPlayer url='https://www.youtube.com/watch?v=L6yX6Oxy_J8' loop='true' width='100%' controls='true'/>
+        </Card>
+        <Card>
+          <Card.Body>
+            <Card.Text>
+            <Card.Title><h2>Burgers</h2></Card.Title>
+
+              <h4>Time</h4>
+              <p>45 mins</p>
+
+              <h4>Ingredients</h4>
+              <ul>
+                <li>Ground Beef</li>
+                <li>Buns</li>
+                <li>Cheese</li>
+
+              </ul>
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <Launch />
+          </Card.Footer>
+        </Card>
+      </CardGroup>
+      </div>
+
+
+        <div style={{margin: '0 auto', textAlign: 'center', }}>
+          <a href="/agenda"><button style={button}>Recipes</button></a>
+          <a href="/about"><button style={button}>About</button></a>
+        </div>
 
       <footer>
         <Footer />
@@ -23,4 +110,62 @@ export default class Home extends React.Component {
     </div>
   )
 } 
+}
+
+function Launch() {
+  const [modalShow, setModalShow] = React.useState(false);
+
+  return (
+    <>
+      <Button variant="light" onClick={() => setModalShow(true)}>
+        Click here to view full recipe
+      </Button>
+
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+    </>
+  );
+}
+
+function MyVerticallyCenteredModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Burgers
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>Time</h4>
+        <p>45 mins</p>
+
+        <h4>Ingredients</h4>
+        <ul>
+          <li>Ground Beef</li>
+          <li>Buns</li>
+          <li>Cheese</li>
+        </ul>
+
+        <h4>Instructions</h4>
+        <ul>
+          <li>Step 1</li>
+          <li>Step 2</li>
+          <li>Step 3</li>
+          <li>Step 4</li>
+          <li>Step 5</li>
+
+        </ul>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
 }
--- mcr/pages/recipes.js
+++ mcr/pages/recipes.js
@@ -2,6 +2,37 @@ import React from 'react';
 import Head from 'next/head';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import ModalHeader from 'react-bootstrap/ModalHeader';
+import ModalTitle from 'react-bootstrap/ModalTitle';
+import ModalBody from 'react-bootstrap/ModalBody';
+import ReactPlayer from 'react-player';
+
+const img ={
+  backgroundImage: 'url("./garage.jpg"), linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5))',
+  height: '25vh',
+  backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
+  position: 'relative',
+  marginBottom: '25px',
+}
+
+const text ={
+    textAlign: 'center',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    color: 'white',
+}
+
+const recipe ={
+  marginBottom: '25px',
+}
 
 export default class Recipes extends React.Component {
   constructor(props) {
@@ -15,7 +46,94 @@ export default class Recipes extends React.Component {
         <title>Recipes</title>
       </Head>
       <Header />
-      <h1>Recipes</h1>
+      <div style={img}>
+        <div style={text}>
+          <h1>RECIPES</h1>
+        </div>
+      </div>
+
+      <div>
+      <CardGroup style={recipe}>
+        <Card>
+          <ReactPlayer url='https://www.youtube.com/watch?v=L6yX6Oxy_J8' loop='true' width='100%' controls='true'/>
+        </Card>
+        <Card>
+          <Card.Body>
+            <Card.Text>
+            <Card.Title><h2>Burgers</h2></Card.Title>
+
+              <h4>Time</h4>
+              <p>45 mins</p>
+
+              <h4>Ingredients</h4>
+              <ul>
+                <li>Ground Beef</li>
+                <li>Buns</li>
+                <li>Cheese</li>
+
+              </ul>
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <Launch />
+          </Card.Footer>
+        </Card>
+      </CardGroup>
+
+      <CardGroup style={recipe}>
+        <Card>
+          <ReactPlayer url='https://www.youtube.com/watch?v=L6yX6Oxy_J8' loop='true' width='100%' controls='true'/>
+        </Card>
+        <Card>
+          <Card.Body>
+            <Card.Text>
+            <Card.Title><h2>Burgers</h2></Card.Title>
+
+              <h4>Time</h4>
+              <p>45 mins</p>
+
+              <h4>Ingredients</h4>
+              <ul>
+                <li>Ground Beef</li>
+                <li>Buns</li>
+                <li>Cheese</li>
+
+              </ul>
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <Launch />
+          </Card.Footer>
+        </Card>
+      </CardGroup>
+
+      <CardGroup style={recipe}>
+        <Card>
+          <ReactPlayer url='https://www.youtube.com/watch?v=L6yX6Oxy_J8' loop='true' width='100%' controls='true'/>
+        </Card>
+        <Card>
+          <Card.Body>
+            <Card.Text>
+            <Card.Title><h2>Burgers</h2></Card.Title>
+
+              <h4>Time</h4>
+              <p>45 mins</p>
+
+              <h4>Ingredients</h4>
+              <ul>
+                <li>Ground Beef</li>
+                <li>Buns</li>
+                <li>Cheese</li>
+
+              </ul>
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <Launch />
+          </Card.Footer>
+        </Card>
+      </CardGroup>
+      </div>
 
       <footer>
         <Footer />
@@ -23,4 +141,62 @@ export default class Recipes extends React.Component {
     </div>
   )
 } 
+}
+
+function Launch() {
+  const [modalShow, setModalShow] = React.useState(false);
+
+  return (
+    <>
+      <Button variant="light" onClick={() => setModalShow(true)}>
+        Click here to view full recipe
+      </Button>
+
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+    </>
+  );
+}
+
+function MyVerticallyCenteredModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Burgers
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>Time</h4>
+        <p>45 mins</p>
+
+        <h4>Ingredients</h4>
+        <ul>
+          <li>Ground Beef</li>
+          <li>Buns</li>
+          <li>Cheese</li>
+        </ul>
+
+        <h4>Instructions</h4>
+        <ul>
+          <li>Step 1</li>
+          <li>Step 2</li>
+          <li>Step 3</li>
+          <li>Step 4</li>
+          <li>Step 5</li>
+
+        </ul>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
 }
