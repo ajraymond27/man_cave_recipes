b9cb9aa071dc1121f565e286aec1278b7395b1fd
AJ Raymond <antraymo@iu.edu>
2020-08-02 18:17:25 -0500

    test

--- mcr/pages/about.js
+++ mcr/pages/about.js
@@ -69,7 +69,7 @@ export default class About extends React.Component {
         <Card.Img variant="top" src="./jim.jpeg" style={jim}/>
         <Card.Body>
           <Card.Text>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+            <p>Lorem ipsum dolore sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
           </Card.Text>
         </Card.Body>
       </Card>      
