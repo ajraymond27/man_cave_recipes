be65e6457bdfa6528bf14abf7fba79ad72c1f027
AJ Raymond <antraymo@iu.edu>
2020-08-02 18:20:00 -0500

    test

--- mcr/public/gallery/photos.js
+++ mcr/public/gallery/photos.js
@@ -59,66 +59,66 @@ const photos = [
       width: 1,
       height: 1
     }, 
-    // {
-    //   src: './gallery/green_soup.jpg',
-    //   width: 1,
-    //   height: 1
-    // }, 
-    // {
-    //   src: './gallery/capstone4.jpg',
-    //   width: 1,
-    //   height: 1
-    // },    
-    // {
-    //   src: './gallery/idk_pasta.jpg',
-    //   width: 1,
-    //   height: 1
-    // }, 
-    // {
-    //   src: './gallery/international1.jpg',
-    //   width: 1,
-    //   height: 1
-    // }, 
-    // {
-    //   src: './gallery/capstone6.jpg',
-    //   width: 1,
-    //   height: 1
-    // },    
-    // {
-    //   src: './gallery/international2.jpg',
-    //   width: 1,
-    //   height: 1
-    // }, 
-    // {
-    //   src: './gallery/lamb.jpg',
-    //   width: 2,
-    //   height: 1
-    // },        
-    // {
-    //   src: './gallery/capstone7.jpg',
-    //   width: 1,
-    //   height: 1
-    // },    
-    // {
-    //   src: './gallery/pasta.jpg',
-    //   width: 1,
-    //   height: 1
-    // }, 
-    // {
-    //   src: './gallery/seneglese_chicken.jpg',
-    //   width: 1,
-    //   height: 1
-    // }, 
-    // {
-    //   src: './gallery/sushi.jpg',
-    //   width: 1,
-    //   height: 1
-    // },      
-    // {
-    //   src: './gallery/capstone5.jpg',
-    //   width: 1,
-    //   height: 1
-    // },            
+    {
+      src: './gallery/green_soup.jpg',
+      width: 1,
+      height: 1
+    }, 
+    {
+      src: './gallery/capstone4.jpg',
+      width: 1,
+      height: 1
+    },    
+    {
+      src: './gallery/idk_pasta.jpg',
+      width: 1,
+      height: 1
+    }, 
+    {
+      src: './gallery/international1.jpg',
+      width: 1,
+      height: 1
+    }, 
+    {
+      src: './gallery/capstone6.jpg',
+      width: 1,
+      height: 1
+    },    
+    {
+      src: './gallery/international2.jpg',
+      width: 1,
+      height: 1
+    }, 
+    {
+      src: './gallery/lamb.jpg',
+      width: 2,
+      height: 1
+    },        
+    {
+      src: './gallery/capstone7.jpg',
+      width: 1,
+      height: 1
+    },    
+    {
+      src: './gallery/pasta.jpg',
+      width: 1,
+      height: 1
+    }, 
+    {
+      src: './gallery/seneglese_chicken.jpg',
+      width: 1,
+      height: 1
+    }, 
+    {
+      src: './gallery/sushi.jpg',
+      width: 1,
+      height: 1
+    },      
+    {
+      src: './gallery/capstone5.jpg',
+      width: 1,
+      height: 1
+    },            
 
   ];
 
